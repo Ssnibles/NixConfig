0500dc7328c87b4c7c6aa3bae07d299feaf9f53a
Ssnibles <joshua.breite@gmail.com>
2026-04-13 10:27:28 +1200

    feat(neovim): add dev

--- nvim/lua/plugins/dev.lua
+++ nvim/lua/plugins/dev.lua
@@ -1,7 +1,12 @@
-local dev_path = "/home/josh/projects/switcheroo/"
+local dev_path = "/home/josh/projects/switcheroo"
 
 if vim.fn.isdirectory(dev_path) == 1 then
 	vim.opt.rtp:prepend(dev_path)
 end
 
-require("switcheroo")
+local ok, err = pcall(require, "switcheroo")
+if not ok then
+	vim.schedule(function()
+		vim.notify(("switcheroo unavailable: %s"):format(err), vim.log.levels.WARN)
+	end)
+end
--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -17,7 +17,7 @@ trouble.setup({
 
 local map = vim.keymap.set
 map("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", { desc = "Trouble diagnostics" })
-map("n", "<leader>xw", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", { desc = "Trouble workspace" })
+map("n", "<leader>xw", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", { desc = "Trouble buffer diagnostics" })
 map("n", "<leader>xq", "<cmd>Trouble qflist toggle<CR>", { desc = "Trouble quickfix" })
 map("n", "<leader>xl", "<cmd>Trouble loclist toggle<CR>", { desc = "Trouble loclist" })
 map("n", "<leader>xs", "<cmd>Trouble symbols toggle focus=false<CR>", { desc = "Trouble symbols" })
