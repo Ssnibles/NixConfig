a6c2574c826555a57935ac86adb0910d38932075
Ssnibles <joshua.breite@gmail.com>
2026-05-26 11:06:32 +1200

    Add stylix support for kando

--- users/josh/default.nix
+++ users/josh/default.nix
@@ -45,6 +45,7 @@
     ../../modules/home/packages.nix
     ../../modules/home/programs.nix
     ../../modules/home/stylix.nix
+    ../../modules/home/kando.nix
     ../../modules/home/scripts.nix
   ];
 
