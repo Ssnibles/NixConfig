1563fa4f310ef7471ff292fb07fefb14c022a01e
Ssnibles <joshua.breite@gmail.com>
2026-05-31 16:23:22 +1200

    Fix the shell history for fish

--- modules/home/shell/fish.nix
+++ modules/home/shell/fish.nix
@@ -90,15 +90,6 @@ in
           fzf_configure_bindings --directory=\cf --git_status=\cg --history=\cr --processes=\cp --variables=\cv
       end
 
-      function __fish_merge_history --on-event fish_prompt
-          history merge
-      end
-
-      function fish_user_key_bindings
-          bind -k up history-search-backward
-          bind -k down history-search-forward
-      end
-
       function petpick --description "Insert a pet snippet selected with fzf"
           if not type -q pet
               echo "petpick: pet is not installed" >&2
