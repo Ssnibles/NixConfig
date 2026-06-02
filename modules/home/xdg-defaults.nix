de9e5a2266d2f3d279d0d9264a7a90d6deb098b7
Ssnibles <joshua.breite@gmail.com>
2026-06-03 01:08:22 +1200

    Set zen to be the default for all things

--- modules/home/xdg-defaults.nix
+++ modules/home/xdg-defaults.nix
@@ -10,18 +10,18 @@
     enable = true;
     defaultApplications = {
     # ── Web / Browser ──────────────────────────────────────────────────────
-    "text/html" = "firefox.desktop";
-    "application/xhtml+xml" = "firefox.desktop";
-    "application/xml" = "firefox.desktop";
-    "x-scheme-handler/http" = "firefox.desktop";
-    "x-scheme-handler/https" = "firefox.desktop";
-    "x-scheme-handler/ftp" = "firefox.desktop";
-    "x-scheme-handler/chrome" = "firefox.desktop";
-    "application/x-extension-htm" = "firefox.desktop";
-    "application/x-extension-html" = "firefox.desktop";
-    "application/x-extension-shtml" = "firefox.desktop";
-    "application/x-extension-xhtml" = "firefox.desktop";
-    "application/x-extension-xht" = "firefox.desktop";
+    "text/html" = "zen.desktop";
+    "application/xhtml+xml" = "zen.desktop";
+    "application/xml" = "zen.desktop";
+    "x-scheme-handler/http" = "zen.desktop";
+    "x-scheme-handler/https" = "zen.desktop";
+    "x-scheme-handler/ftp" = "zen.desktop";
+    "x-scheme-handler/chrome" = "zen.desktop";
+    "application/x-extension-htm" = "zen.desktop";
+    "application/x-extension-html" = "zen.desktop";
+    "application/x-extension-shtml" = "zen.desktop";
+    "application/x-extension-xhtml" = "zen.desktop";
+    "application/x-extension-xht" = "zen.desktop";
 
     # ── PDF / E-book ───────────────────────────────────────────────────────
     "application/pdf" = "sioyek.desktop";
