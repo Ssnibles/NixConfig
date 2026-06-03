d5c4dd20bcbbfc07f6642865b6daef8e2f9d9b72
Ssnibles <joshua.breite@gmail.com>
2026-06-03 14:32:53 +1200

    Disable inactive dimming in focus mode

--- modules/home/scripts.nix
+++ modules/home/scripts.nix
@@ -73,6 +73,7 @@ let
       ${pkgs.hyprland}/bin/hyprctl keyword general:gaps_in $GAPS_IN
       ${pkgs.hyprland}/bin/hyprctl keyword general:gaps_out $GAPS_OUT
       ${pkgs.hyprland}/bin/hyprctl keyword decoration:rounding $ROUNDING
+      ${pkgs.hyprland}/bin/hyprctl keyword decoration:dim_inactive true
       qs -c hyprland ipc call bar toggle 2>/dev/null || true
       echo "normal" > "$STATE_FILE"
       ${pkgs.libnotify}/bin/notify-send "Focus Mode" "Disabled - Normal mode restored"
@@ -80,6 +81,7 @@ let
       ${pkgs.hyprland}/bin/hyprctl keyword general:gaps_in 0
       ${pkgs.hyprland}/bin/hyprctl keyword general:gaps_out 0
       ${pkgs.hyprland}/bin/hyprctl keyword decoration:rounding 0
+      ${pkgs.hyprland}/bin/hyprctl keyword decoration:dim_inactive false
       qs -c hyprland ipc call bar toggle 2>/dev/null || true
       echo "focus" > "$STATE_FILE"
       ${pkgs.libnotify}/bin/notify-send "Focus Mode" "Enabled - Distractions removed"
