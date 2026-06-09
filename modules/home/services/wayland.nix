994e6f53d9d795dc028e99b87eb4c361018bcf3f
Ssnibles <joshua.breite@gmail.com>
2026-06-09 16:36:22 +1200

    Fix all the errors from migration

--- modules/home/services/wayland.nix
+++ modules/home/services/wayland.nix
@@ -37,11 +37,13 @@
       Description = "Awww wallpaper daemon";
       PartOf = [ "graphical-session.target" ];
       After = [ "graphical-session.target" ];
+      StartLimitBurst = 10;
+      StartLimitIntervalSec = 60;
     };
     Service = {
       ExecStart = "${pkgs.bash}/bin/bash -lc 'for i in {1..50}; do for socket in \"$XDG_RUNTIME_DIR\"/wayland-*; do if [ -S \"$socket\" ]; then export WAYLAND_DISPLAY=\"$(basename \"$socket\")\"; exec ${pkgs.unstable.awww}/bin/awww-daemon; fi; done; sleep 0.1; done; echo \"Awww: Wayland socket not ready\" >&2; exit 1'";
-      Restart = "on-failure";
-      RestartSec = 1;
+      Restart = "always";
+      RestartSec = 2;
     };
     Install.WantedBy = [ "graphical-session.target" ];
   };
