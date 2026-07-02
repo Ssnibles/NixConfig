e325fc4ae54b6a2055d347fc06c58e87cf26c42d
Ssnibles <joshua.breite@gmail.com>
2026-07-03 02:00:30 +1200

    Switch to tuigreet

--- modules/nixos/desktop/display-manager.nix
+++ modules/nixos/desktop/display-manager.nix
@@ -1,20 +1,13 @@
 { pkgs, ... }:
 
 {
-  services.displayManager.ly = {
+  services.greetd = {
     enable = true;
+    useTextGreeter = true;
     settings = {
-      box_title = "Welcome";
-      clock = "%a %d %b  %H:%M";
-      text_in_center = true;
-      box_position_v = 0.50;
-      input_len = 38;
-      default_input = "login";
-      hide_key_hints = true;
-      hide_keyboard_locks = true;
-      hide_version_string = true;
-      animation = "none";
-      blank_box = true;
+      default_session = {
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+      };
     };
   };
 
@@ -24,6 +17,4 @@
   };
 
   services.gnome.gnome-keyring.enable = true;
-  security.pam.services.ly.enableGnomeKeyring = true;
-  security.pam.services.login.enableGnomeKeyring = true;
 }
--- modules/nixos/desktop/hyprland.nix
+++ modules/nixos/desktop/hyprland.nix
@@ -6,5 +6,9 @@
   # Tell applications (Discord/Vesktop, browsers, etc.) to use the Hyprland
   # portal backend for screen casting. Without this, screen sharing via
   # xdg-desktop-portal won't work on Wayland.
-  environment.sessionVariables.XDG_CURRENT_DESKTOP = "Hyprland";
+  environment.sessionVariables = {
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    NIXOS_OZONE_WL = "1";
+    ELECTRON_OZONE_PLATFORM_HINT = "wayland";
+  };
 }
