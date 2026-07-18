81d974eb56ec56013bf960924d1357be524b8df4
Ssnibles <joshua.breite@gmail.com>
2026-07-18 21:19:44 +1200

    Update install script

--- modules/hosts/desktop/hardware-configuration.nix
+++ modules/hosts/desktop/hardware-configuration.nix
@@ -26,23 +26,9 @@
       boot.kernelModules = [ "kvm-amd" ];
       boot.extraModulePackages = [ ];
 
-      fileSystems."/" = {
-        device = "/dev/disk/by-uuid/7001a8e1-003d-4119-afbb-5d6d25759e08";
-        fsType = "ext4";
-      };
-
-      fileSystems."/boot" = {
-        device = "/dev/disk/by-uuid/5752-EAAD";
-        fsType = "vfat";
-        options = [
-          "fmask=0077"
-          "dmask=0077"
-        ];
-      };
-
-      swapDevices = [
-        { device = "/dev/disk/by-uuid/e87cbcca-c7c6-4e03-90e7-5852ab59f0b9"; }
-      ];
+      # NOTE: fileSystems and swapDevices are provided by the generated
+      # ./_hardware-generated.nix (created by install.sh at install time
+      # via nixos-generate-config). Do not hard-code UUIDs here.
 
       nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
       hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
--- modules/hosts/laptop/hardware-configuration.nix
+++ modules/hosts/laptop/hardware-configuration.nix
@@ -25,30 +25,11 @@
       boot.kernelModules = [ "kvm-amd" ];
       boot.extraModulePackages = [ ];
 
-      fileSystems."/" = {
-        device = "/dev/disk/by-uuid/78d511b1-8932-4af7-b74d-8194aeef0903";
-        fsType = "ext4";
-      };
+      # NOTE: fileSystems and swapDevices are provided by the generated
+      # ./_hardware-generated.nix (created by install.sh at install time
+      # via nixos-generate-config). Do not hard-code UUIDs here.
 
-      fileSystems."/boot" = {
-        device = "/dev/disk/by-uuid/776B-1FD9";
-        fsType = "vfat";
-        options = [
-          "fmask=0077"
-          "dmask=0077"
-        ];
-      };
-
-      swapDevices = [
-        { device = "/dev/disk/by-uuid/a0077d83-9d7b-4ecd-b60b-2de46562d841"; }
-      ];
-
-      # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-      # (the default) this is the recommended approach. When using systemd-networkd it's
-      # still possible to use this option, but it's recommended to use it in conjunction
-      # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
       networking.useDHCP = lib.mkDefault true;
-      # networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
 
       nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
       hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
