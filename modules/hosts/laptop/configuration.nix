6eb6d945cf2892839acfb62ba22fe2f8ca986aed
Ssnibles <joshua.breite@gmail.com>
2026-07-23 21:15:55 +1200

    feat: include startup module in hosts

--- modules/hosts/laptop/configuration.nix
+++ modules/hosts/laptop/configuration.nix
@@ -5,6 +5,7 @@
     {
       imports = [
         self.nixosModules.base
+        self.nixosModules.startup
         self.nixosModules.cli
         self.nixosModules.communications
         self.nixosModules.fonts
