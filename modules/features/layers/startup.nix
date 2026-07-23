575aac320707f6bcdac0140aaffd28e4d82ba8c4
Ssnibles <joshua.breite@gmail.com>
2026-07-23 21:15:39 +1200

    feat: add startup services module and add vicinae

---
+++ modules/features/layers/startup.nix
@@ -0,0 +1,18 @@
+{ self, inputs, ... }:
+{
+  flake.nixosModules.startup =
+    { pkgs, lib, config, ... }:
+    {
+      systemd.user.services.vicinae-server = {
+        description = "Vicinae application launcher server";
+        wantedBy = [ "graphical-session.target" ];
+        partOf = [ "graphical-session.target" ];
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.vicinae}/bin/vicinae server";
+          Restart = "on-failure";
+          RestartSec = 2;
+        };
+      };
+    };
+}
