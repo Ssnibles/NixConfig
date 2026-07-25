541f7ee49b5d1f908a27a07848351e7156ecdf5a
Ssnibles <joshua.breite@gmail.com>
2026-07-25 21:31:14 +1200

    feat(niri): update a bunch of niri stuff

--- modules/hosts/laptop/configuration.nix
+++ modules/hosts/laptop/configuration.nix
@@ -32,7 +32,7 @@
       ];
 
       # Bootloader
-      boot.loader.systemd-boot.enable = true;
+      boot.loader.limine.enable = true;
       boot.loader.efi.canTouchEfiVariables = true;
 
       # Use latest kernel
