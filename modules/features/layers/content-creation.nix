54a8ce8fdbf0c96f2b8d110e5b49c08825d70f46
jb878 <jb878@students.waikato.ac.nz>
2026-07-30 19:37:11 +1200

    chore: remove obs and other things

--- modules/features/layers/content-creation.nix
+++ modules/features/layers/content-creation.nix
@@ -11,19 +11,8 @@
             spek
             ffmpeg
             flac
-            (pkgs.wrapOBS {
-              plugins = with pkgs.obs-studio-plugins; [
-                wlrobs
-                obs-backgroundremoval
-                obs-pipewire-audio-capture
-                obs-vaapi
-                obs-gstreamer
-                obs-vkcapture
-              ];
-            })
           ]
           ++ (with pkgs.unstable; [
-            chatterino7
             gimp3
           ]);
       };
