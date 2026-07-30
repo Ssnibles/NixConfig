2e3d0938e8d9008ff0d299f278436bdb3f27eb38
Ssnibles <joshua.breite@gmail.com>
2026-07-31 11:06:16 +1200

    refactor(hjem): convert terminal file declarations to expanded set syntax

--- modules/packages/foot/default.nix
+++ modules/packages/foot/default.nix
@@ -30,37 +30,41 @@
 
       hjem.users."${config.username}" = {
         files = {
-          ".config/foot/foot.ini".text = ''
-            include=/home/${config.username}/.config/foot/colors.ini
-            font=JetBrainsMono Nerd Font:size=12
-            pad=10x10
+          ".config/foot/foot.ini" = {
+            text = ''
+              include=/home/${config.username}/.config/foot/colors.ini
+              font=JetBrainsMono Nerd Font:size=12
+              pad=10x10
 
-            [cursor]
-            style=block
-          '';
-          ".config/foot/colors.ini".text = ''
-            [colors-dark]
-            background=${bg}
-            foreground=${fg}
-            selection-foreground=${bg}
-            selection-background=${accent}
-            regular0=${bg}
-            regular1=${red}
-            regular2=${green}
-            regular3=${yellow}
-            regular4=${accent}
-            regular5=${purple}
-            regular6=${teal}
-            regular7=${fg}
-            bright0=${bgRaised}
-            bright1=${orange}
-            bright2=${green}
-            bright3=${yellow}
-            bright4=${accent}
-            bright5=${purple}
-            bright6=${teal}
-            bright7=${fg}
-          '';
+              [cursor]
+              style=block
+            '';
+          };
+          ".config/foot/colors.ini" = {
+            text = ''
+              [colors-dark]
+              background=${bg}
+              foreground=${fg}
+              selection-foreground=${bg}
+              selection-background=${accent}
+              regular0=${bg}
+              regular1=${red}
+              regular2=${green}
+              regular3=${yellow}
+              regular4=${accent}
+              regular5=${purple}
+              regular6=${teal}
+              regular7=${fg}
+              bright0=${bgRaised}
+              bright1=${orange}
+              bright2=${green}
+              bright3=${yellow}
+              bright4=${accent}
+              bright5=${purple}
+              bright6=${teal}
+              bright7=${fg}
+            '';
+          };
         };
       };
     };
--- modules/packages/kitty/default.nix
+++ modules/packages/kitty/default.nix
@@ -11,71 +11,75 @@
 
     hjem.users."${config.username}" = {
       files = {
-        ".config/kitty/kitty.conf".text = ''
-          confirm_os_window_close 0
+        ".config/kitty/kitty.conf" = {
+          text = ''
+            confirm_os_window_close 0
 
-          font_family      MartianMono Nerd Font
-          bold_font        auto
-          italic_font      MartianMono Nerd Font Italic
-          bold_italic_font MartianMono Nerd Font Bold Italic
-          font_size        12.0
+            font_family      MartianMono Nerd Font
+            bold_font        auto
+            italic_font      MartianMono Nerd Font Italic
+            bold_italic_font MartianMono Nerd Font Bold Italic
+            font_size        12.0
 
-          include colors.conf
-        '';
+            include colors.conf
+          '';
+        };
 
-        ".config/kitty/colors.conf".text = ''
-          # vim:ft=kitty
+        ".config/kitty/colors.conf" = {
+          text = ''
+            # vim:ft=kitty
 
-          foreground              #${fg}
-          background              #${bg}
-          selection_foreground    #${bg}
-          selection_background    #${accent}
+            foreground              #${fg}
+            background              #${bg}
+            selection_foreground    #${bg}
+            selection_background    #${accent}
 
-          cursor                  #${fg}
-          cursor_text_color       #${bg}
+            cursor                  #${fg}
+            cursor_text_color       #${bg}
 
-          scrollbar_handle_color  #606079
-          scrollbar_track_color   #${bgRaised}
+            scrollbar_handle_color  #606079
+            scrollbar_track_color   #${bgRaised}
 
-          url_color               #${accent}
+            url_color               #${accent}
 
-          active_border_color     #${accent}
-          inactive_border_color   #252530
-          bell_border_color       #${yellow}
+            active_border_color     #${accent}
+            inactive_border_color   #252530
+            bell_border_color       #${yellow}
 
-          wayland_titlebar_color system
-          macos_titlebar_color system
+            wayland_titlebar_color system
+            macos_titlebar_color system
 
-          active_tab_foreground   #${bg}
-          active_tab_background   #${accent}
-          inactive_tab_foreground #${fg}
-          inactive_tab_background #${bgRaised}
-          tab_bar_background      #${bg}
+            active_tab_foreground   #${bg}
+            active_tab_background   #${accent}
+            inactive_tab_foreground #${fg}
+            inactive_tab_background #${bgRaised}
+            tab_bar_background      #${bg}
 
-          mark1_foreground #${bg}
-          mark1_background #${accent}
-          mark2_foreground #${bg}
-          mark2_background #${purple}
-          mark3_foreground #${bg}
-          mark3_background #${teal}
+            mark1_foreground #${bg}
+            mark1_background #${accent}
+            mark2_foreground #${bg}
+            mark2_background #${purple}
+            mark3_foreground #${bg}
+            mark3_background #${teal}
 
-          color0  #${bg}
-          color8  #${bgRaised}
-          color1  #${red}
-          color9  #${orange}
-          color2  #${green}
-          color10 #${green}
-          color3  #${yellow}
-          color11 #${yellow}
-          color4  #${accent}
-          color12 #${accent}
-          color5  #${purple}
-          color13 #${purple}
-          color6  #${teal}
-          color14 #${teal}
-          color7  #${fg}
-          color15 #${fg}
-        '';
+            color0  #${bg}
+            color8  #${bgRaised}
+            color1  #${red}
+            color9  #${orange}
+            color2  #${green}
+            color10 #${green}
+            color3  #${yellow}
+            color11 #${yellow}
+            color4  #${accent}
+            color12 #${accent}
+            color5  #${purple}
+            color13 #${purple}
+            color6  #${teal}
+            color14 #${teal}
+            color7  #${fg}
+            color15 #${fg}
+          '';
+        };
       };
     };
   };
