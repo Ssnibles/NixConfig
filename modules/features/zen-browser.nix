1fc0bab8efa9264e29e227f13a297e0426b17f7b
Ssnibles <joshua.breite@gmail.com>
2026-08-11 22:55:30 +1200

    set proper preferences

--- modules/features/zen-browser.nix
+++ modules/features/zen-browser.nix
@@ -14,6 +14,30 @@
       programs.firefox = {
         enable = true;
         package = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default;
+
+        preferences = {
+          "webgl.disabled" = false;
+          "privacy.resistFingerprinting" = false;
+          "privacy.clearOnShutdown.history" = false;
+          "privacy.clearOnShutdown.cookies" = false;
+          "network.cookie.lifetimePolicy" = 0;
+          "cookiebanners.service.mode.privateBrowsing" = 2;
+          "cookiebanners.service.mode" = 2;
+          "privacy.donottrackheader.enabled" = true;
+          "privacy.fingerprintingProtection" = true;
+          "privacy.trackingprotection.emailtracking.enabled" = true;
+          "privacy.trackingprotection.enabled" = true;
+          "privacy.trackingprotection.fingerprinting.enabled" = true;
+          "privacy.trackingprotection.socialtracking.enabled" = true;
+          "browser.startup.homepage" = "https://startpage.com";
+          "browser.tabs.unloadOnLowMemory" = true;
+          "zen.view.compact.show-sidebar-and-toolbar-on-hover" = true;
+          "browser.sessionstore.interval" = 600000;
+          "zen.theme.content-element-separation" = 0;
+          "zen.theme.border-radius" = 0;
+          "widget.gtk.rounded-bottom-corners.enabled" = false;
+        };
+
         policies = {
           SearchEngines = {
             Default = "DuckDuckGo";
@@ -21,28 +45,6 @@
           };
           DisableTelemetry = true;
           DisableFirefoxStudies = true;
-          Preferences = {
-            "webgl.disabled" = false;
-            "privacy.resistFingerprinting" = false;
-            "privacy.clearOnShutdown.history" = false;
-            "privacy.clearOnShutdown.cookies" = false;
-            "network.cookie.lifetimePolicy" = 0;
-            "cookiebanners.service.mode.privateBrowsing" = 2;
-            "cookiebanners.service.mode" = 2;
-            "privacy.donottrackheader.enabled" = true;
-            "privacy.fingerprintingProtection" = true;
-            "privacy.trackingprotection.emailtracking.enabled" = true;
-            "privacy.trackingprotection.enabled" = true;
-            "privacy.trackingprotection.fingerprinting.enabled" = true;
-            "privacy.trackingprotection.socialtracking.enabled" = true;
-            "browser.startup.homepage" = "https://startpage.com";
-            "browser.tabs.unloadOnLowMemory" = true;
-            "zen.view.compact.show-sidebar-and-toolbar-on-hover" = true;
-            "browser.sessionstore.interval" = 600000;
-            "zen.theme.content-element-separation" = 0;
-            "zen.theme.border-radius" = 0;
-            "widget.gtk.rounded-bottom-corners.enabled" = false;
-          };
           ExtensionSettings = {
             "uBlock0@raymondhill.net" = {
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
