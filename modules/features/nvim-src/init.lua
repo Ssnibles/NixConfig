278dc2c1314eb1f17b06d99c3cdb97ca597d7ab2
Ssnibles <joshua.breite@gmail.com>
2026-08-22 18:55:29 +1200

    feat(neovim): Remove repl-icate.nvim

--- modules/features/nvim-src/init.lua
+++ modules/features/nvim-src/init.lua
@@ -4,7 +4,7 @@ for _, dev_plugin in ipairs({
 	"/home/josh/sshinator.nvim",
 	"/home/josh/indentinator.nvim",
 	"/home/josh/zline.nvim",
-	"/home/josh/repl-icate.nvim",
+	-- "/home/josh/repl-icate.nvim",
 }) do
 	if vim.uv.fs_stat(dev_plugin) then
 		vim.opt.runtimepath:append(dev_plugin)
