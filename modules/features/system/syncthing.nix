6c6f78a20e5c8c08af0139afae2e1d9eebd3b364
Ssnibles <joshua.breite@gmail.com>
2026-09-18 12:53:53 +1200

    perf(system): defer syncthing to graphical.target to eliminate boot delays

--- modules/features/system/syncthing.nix
+++ modules/features/system/syncthing.nix
@@ -21,7 +21,35 @@
         guiAddress = "127.0.0.1:8384";
         openDefaultPorts = true;
 
+        # Keep dynamically added or auto-accepted devices and folders
+        overrideDevices = false;
+        overrideFolders = false;
+
         settings = {
+          devices = {
+            "homeserver" = {
+              id = "4V7XZFM-QEHVMHV-OYQXCV2-WBDIHJI-WYOPD46-KHIOD22-352EP6X-FY3DEAB";
+              addresses = [
+                "tcp://homeserver:22000"
+                "dynamic"
+              ];
+              autoAcceptFolders = true;
+            };
+          };
+
+          folders = {
+            "Documents" = {
+              id = "pfqwn-dke5x";
+              path = "/home/${config.username}/Documents";
+              devices = [ "homeserver" ];
+            };
+            "Hermes" = {
+              id = "tuyqy-adrau";
+              path = "/home/${config.username}/Hermes";
+              devices = [ "homeserver" ];
+            };
+          };
+
           options = {
             # Laptop battery optimization: disable battery-draining internet relaying & global discovery
             globalAnnounceEnabled = false;
@@ -29,11 +57,18 @@
             natEnabled = false;
             localAnnounceEnabled = true; # Direct discovery on local Wi-Fi
             urAccepted = -1; # Disable anonymous usage reporting
+            defaultFolderPath = "~";
           };
           gui = {
             theme = "dark";
           };
         };
       };
+
+      systemd.services.syncthing.serviceConfig.WorkingDirectory = "/home/${config.username}";
+
+      # Defer syncthing to graphical.target to eliminate ~1.5s blocking delay during early boot
+      systemd.services.syncthing.wantedBy = lib.mkForce [ "graphical.target" ];
+      systemd.services.syncthing-init.wantedBy = lib.mkForce [ "graphical.target" ];
     };
 }
